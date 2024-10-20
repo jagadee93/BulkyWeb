f113b1913cb0d314264e12e9674f4654f5ff8904
jagadee93 <jagadeeshgoud001@gmail.com>
2024-10-20 22:02:57 +0530

    Added ASP.net config files and basic model and DB Context files

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyWeb.Models
+{
+    public class Category
+    {
+       public required int Id { get; set; }
+       public required string Name { get; set; }
+       public int DisplayOrder { get; set; }
+    }
+}
